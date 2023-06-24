a04a9f05dbe54abf7d7dda613514577e0379734a
Daniel <darodweb@gmail.com>
2023-06-24 13:58:08 -0300

    Initial commit

---
+++ camino-al-despertar/src/App.vue
@@ -0,0 +1,53 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+
+    <div class="wrapper">
+
+      <nav>
+        <!-- <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink> -->
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+  max-height: 100vh;
+}
+
+
+nav a.router-link-exact-active {
+  color: var(--color-text);
+}
+
+nav a.router-link-exact-active:hover {
+  background-color: transparent;
+}
+
+nav a {
+  display: inline-block;
+  padding: 0 1rem;
+  border-left: 1px solid var(--color-border);
+}
+
+nav a:first-of-type {
+  border: 0;
+}
+
+/* @media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+} */
+</style>
---
+++ camino-al-despertar/src/views/LinksView.vue
@@ -0,0 +1,28 @@
+<template>
+
+    <div class="page-bg">
+        
+    </div>
+
+
+</template>
+
+<script>
+export default {
+    name: 'LinksView'
+}
+</script>
+
+<style scoped>
+
+.page-bg {
+    background-image: url('../assets/Home-Page-background.webp');
+    background-position: bottom center;
+    background-attachment: scroll;
+    min-height: 100vh;
+    min-width: 100vw;
+
+}
+
+
+</style>
